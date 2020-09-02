b077c8e24882554001124a6f89382de6fe680535
jatai <565735954@qq.com>
2020-09-02 15:32:16 +0800

    css sass loader

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -2,16 +2,31 @@ const path = require('path');
 
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
+  // mode: 'development',
   entry: './src/index.tsx',
   output: {
     path: path.resolve('./dist/'),
     filename: 'bundle.js'
   },
-  resolve: { extensions: ["*", ".js", ".jsx", ".tx", ".tsx"] },
+  resolve: { extensions: ['*', '.js', '.jsx', '.tx', '.tsx'] },
   module: {
     rules: [
-      { test: /\.(js|ts)x?$/, exclude: /node_modules/, loader: "babel-loader" }
+      {
+        test: /\.(js|ts)x?$/,
+        exclude:  path.resolve('node_modules/'),
+        include:  path.resolve('src/'),
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+
+        test: /\.(sass|scss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
     ]
   }
 
---
+++ src/app.tsx
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+import './app.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1> React tsx from webpack! </h1>
+      </div>
+    );
+  }
+}
+
+export default App;
