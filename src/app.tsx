93da2ebf44fc5ade6e95de1d2f97a2fe15bf45bb
jatai <565735954@qq.com>
2020-09-07 14:49:08 +0800

    code split

--- src/app.tsx
+++ src/app.tsx
@@ -2,37 +2,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Layout } from 'antd';
+import loadable from '@loadable/component'
 
 import Header from './components/header/header';
 import Aside from './components/aside/aside';
-import Module1 from './modules/module1';
-import Module2 from './modules/module2';
-
 import './app.scss';
+
+const Login = loadable(() => import('./modules/login/login'));
+const Home = loadable(() => import('./components/home/home'));
 
 class App extends React.Component {
 
   render() {
     return (
       <Router>
-        <Layout style={{ minHeight: '100vh' }}>
-          <Header />
-          <Layout>
-            <Aside />
-            <Layout className="border-left">
-              <Layout.Content className="p-2 bg-white">
-                <Switch>
-                  <Route exact path="/module1">
-                    <Module1 />
-                  </Route>
-                  <Route path="/module2">
-                    <Module2 />
-                  </Route>
-                </Switch>
-              </Layout.Content>
-            </Layout>
-          </Layout>
-        </Layout>
+        <Switch>
+          <Route  path="/login" component={Login} />
+          <Route path="/home" component={Home} />
+        </Switch>
       </Router>
 
     );
--- src/modules/module1.tsx
+++ src/modules/module1.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 
-const Module1 = ()=>(
-  <div>module1</div>
-);
+
+class Module1 extends React.Component {
+
+  render() {
+    return (
+      <p>Module31</p>
+    );
+  }
+}
+// const Module1 = () => (
+//   <div>module1</div>
+// );
 export default Module1;
